935596e6942c947cdb94fa00c988fcd7e6b45268
foresitegroup <mark@foresitegrp.com>
2016-01-21 17:01:22 -0600

    2016-01-21

--- wp-content/themes/labmidwest/content-home.php
+++ wp-content/themes/labmidwest/content-home.php
@@ -7,3 +7,19 @@
   	<?php the_content(); ?>
   </div>
 </div>
+
+<div class="home-product-video">
+  <div class="home-video-background"<?php if (get_post_thumbnail_id(15) != "") echo " style=\"background-image: url(" . wp_get_attachment_url(get_post_thumbnail_id(15)) . ");\""; ?>>
+    <div class="overlay"></div>
+  </div>
+  <div class="site-width">
+    <div class="home-featured-product">
+      <?php echo do_shortcode('[insert page="featured-product" display="content"]'); ?>
+    </div>
+    <div class="home-video">
+      <?php echo do_shortcode('[insert page="video" display="content"]'); ?>
+    </div>
+  </div>
+</div>
+
+Oh what a hack....
--- wp-content/themes/labmidwest/functions.php
+++ wp-content/themes/labmidwest/functions.php
@@ -1,4 +1,7 @@
 <?php
+// We want Featured Images on Pages and Posts
+add_theme_support( 'post-thumbnails' );
+
 // Define menus
 function register_my_menus() {
   register_nav_menus(
@@ -8,4 +11,15 @@ function register_my_menus() {
   );
 }
 add_action( 'init', 'register_my_menus' );
+
+// Make videos autoplay with Responsive Lightbox / Swipebox
+add_action( 'wp_enqueue_scripts', 'enqueue_and_register_my_scripts' );
+function enqueue_and_register_my_scripts(){
+  wp_deregister_script(
+    'responsive-lightbox-swipebox', plugins_url( 'assets/swipebox/js/jquery.swipebox.min.js', __FILE__ ), array( 'jquery' )
+  );
+  wp_register_script(
+    'responsive-lightbox-swipebox', get_stylesheet_directory_uri() . '/inc/jquery.swipebox.min.js', array( 'jquery' )
+  );
+}
 ?>
