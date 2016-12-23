2d592f314766a9125ed7635729fdfe3bdba971d7
foresitegroup <mark@foresitegrp.com>
2016-12-23 13:09:06 -0600

    Add template

---
+++ wp-content/themes/labmidwest/template-categories.php
@@ -0,0 +1,48 @@
+<?php
+/* Template Name: Category */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
+
+  <?php
+  // Start the loop.
+  while ( have_posts() ) : the_post();
+  ?>
+
+    <div class="banner banner-category">
+      <div class="site-width-left">
+        <div class="site-width-left-content">
+          <h1><?php echo get_the_title(); ?></h1>
+          <?php the_subtitle(); ?>
+        </div>
+      </div>
+      <div class="site-width-right"<?php if (get_post_thumbnail_id() != "") echo " style=\"background-image: url(" . wp_get_attachment_url(get_post_thumbnail_id()) . ");\""; ?>>
+      </div>
+      <div style="clear: both;"></div>
+    </div>
+
+    <?php the_content(); ?>
+
+  <?php
+  // End the loop.
+  endwhile;
+  ?>
+
+  </main><!-- .site-main -->
+
+  <div id="rmi" class="rmi-category">
+    <div class="site-width">
+      <div class="rmi-header">
+        <h2>FOR MORE INFORMATION</h2>
+        Contact us directly at (888) 123-4567 or fill out the form below.
+      </div>
+
+      <?php if(function_exists('ninja_forms_display_form')){ ninja_forms_display_form(5); } ?>
+    </div>
+  </div>
+
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
