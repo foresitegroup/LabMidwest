78508a8aa60ac6244412817716fcdc0fddd12819
foresitegroup <mark@foresitegrp.com>
2020-03-17 12:14:17 -0500

    Changes made by LabMidwest

--- wp-content/themes/labmidwest/footer.php
+++ wp-content/themes/labmidwest/footer.php
@@ -1,34 +1,42 @@
-
 <?php wp_footer(); ?>
 
 <div class="footer">
   <div class="site-width">
     <div class="footer-form">
-      <h2>SUBSCRIBE</h2>
-      Stay up to date on industry news &amp; events.<br>
+      <h2>STAY UPDATED!</h2>
+      We'll keep you informed on the latest news in education and workforce training<br>
       <br>
 
-      <!-- Begin MailChimp Signup Form -->
-      <div id="mc_embed_signup">
-        <form action="//labmidwest.us3.list-manage.com/subscribe/post?u=0a4c9e5c39e55cdc38cda3b97&amp;id=fbe781d09c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-          <div id="mc_embed_signup_scroll">
-            <div class="mc-field-group">
-              <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="EMAIL ADDRESS">
-            </div>
+<!-- Begin Mailchimp Signup Form -->
+<div id="mc_embed_signup">
+<form action="https://labmidwest.us3.list-manage.com/subscribe/post?u=0a4c9e5c39e55cdc38cda3b97&amp;id=3efb867571" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	<label for="mce-EMAIL">Education News</label>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_0a4c9e5c39e55cdc38cda3b97_3efb867571" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
 
-            <div id="mce-responses" class="clear">
-              <div class="response" id="mce-error-response" style="display:none"></div>
-              <div class="response" id="mce-success-response" style="display:none"></div>
-            </div>
+<!--End mc_embed_signup-->
+<br>
+<!-- Begin Mailchimp Signup Form -->
+<div id="mc_embed_signup">
+<form action="https://labmidwest.us3.list-manage.com/subscribe/post?u=0a4c9e5c39e55cdc38cda3b97&amp;id=9406977e48" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	<label for="mce-EMAIL">Industry &amp; Workforce News</label>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_0a4c9e5c39e55cdc38cda3b97_9406977e48" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
 
-            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_0a4c9e5c39e55cdc38cda3b97_fbe781d09c" tabindex="-1" value=""></div>
+<!--End mc_embed_signup-->
 
-            <div class="clear"><input type="submit" value="SUBMIT" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-          </div>
-        </form>
-      </div>
-      <!--End mc_embed_signup-->
     </div>
 
     <div class="footer-menu">
